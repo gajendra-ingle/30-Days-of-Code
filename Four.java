83c44ee2f02b0d4c8ca787a9eada6880eecde149
gajendra-ingle <gajendraingle01@gmail.com>
2023-10-10 16:13:15 +0530

    Add some comment

--- Four.java
+++ Four.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Four {
 
+    // method for sum of elements in an array
     public static int arraySum(int arr[]) {
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
