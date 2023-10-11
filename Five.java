69e58e8d66742609fd0da33bacf7a8727aed3f81
gajendra-ingle <gajendraingle01@gmail.com>
2023-10-11 10:28:43 +0530

    Fifth Day

--- Five.java
+++ Five.java
@@ -1,3 +1,25 @@
+import java.util.Scanner;
+
 public class Five {
-    
+
+    public static int chechPositiveOrNegative(int num) {
+        if (num > 0) {
+            System.out.println("Positive Number.");
+        } else {
+            System.out.println("Negative Number.");
+        }
+        return num;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number : ");
+        int num = sc.nextInt();
+
+        chechPositiveOrNegative(num);
+
+        sc.close();
+
+    }
 }
