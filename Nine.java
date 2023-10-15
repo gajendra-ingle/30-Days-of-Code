598a332279980bef13caf3e0ba88b106ec0ac4f9
gajendra-ingle <gajendraingle01@gmail.com>
2023-10-15 12:59:18 +0530

    Nine Day

--- Nine.java
+++ Nine.java
@@ -1,3 +1,29 @@
+import java.util.Scanner;
+
 public class Nine {
-    
+
+    // Calculate string length function
+    static int calculateStringLength(String str) {
+        int length = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            length++;
+        }
+        return length;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Taking user input
+        System.out.println("Enter the string : ");
+        String str = sc.nextLine();
+
+        // // Call the calculateStringLength() function.
+        int length = calculateStringLength(str);
+        System.out.println("Length of given string is : " + length);
+
+        sc.close();
+    }
 }
