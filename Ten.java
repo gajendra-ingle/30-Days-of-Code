6ca0340cbebd78c8796f5883f89a3bd3e63a258d
gajendra-ingle <gajendraingle01@gmail.com>
2023-10-16 09:15:24 +0530

    Ten Day

--- Ten.java
+++ Ten.java
@@ -1,3 +1,27 @@
+import java.util.Scanner;
+
 public class Ten {
-    
+
+    // function that check number is multiple of 3 and 5.
+    static void checkMultiples(int num) {
+        if (num % 3 == 0 && num % 5 == 0) {
+            System.out.println("Yes, The given number is multiple of 3 and 5.");
+        }else{
+            System.out.println("No, The given number is not multiple of 3 and 5.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // take user input
+        System.out.print("Enter the number : ");
+        int num = sc.nextInt();
+
+        // call checkMultiple() function
+        checkMultiples(num);
+
+        // Scanner close
+        sc.close();
+    }
 }
