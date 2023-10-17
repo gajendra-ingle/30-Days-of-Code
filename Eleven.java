124b75c912a6a82261be34b301adc02d833ed060
gajendra-ingle <gajendraingle01@gmail.com>
2023-10-17 12:04:51 +0530

    Eleven Day

--- Eleven.java
+++ Eleven.java
@@ -1,3 +1,28 @@
+import java.util.Scanner;
+
 public class Eleven {
-    
+
+    // function that checks the given year is leap or not.
+    static void checkLeapYear(int year){
+        
+        if(year % 4 ==0 && year % 100 != 0 || year % 400 == 0 ){
+              System.out.println("Yes, It's a leap year.");
+        }else{
+            System.out.println("No, It's not a leap year.");
+     
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the year : ");
+        int year = sc.nextInt();
+
+        // call the checkLeapYear() function.
+        checkLeapYear(year);
+
+        sc.close();
+    }
 }
