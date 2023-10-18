0e862eb605ae4dada8ec749848e1512202516f33
gajendra-ingle <gajendraingle01@gmail.com>
2023-10-18 09:20:52 +0530

    Twelve Day

--- Twelve.java
+++ Twelve.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class Twelve {
+
+    // Function that print the fibonacci series.
+    static void printFibonacciSeries(int number) {
+        int a = 0;
+        int b = 1;
+        int c;
+
+        for (int i = 0; i <= number; i++) {
+            System.out.print(a + " ");
+            c = a + b;
+            a = b;
+            b = c;
+        }
+    }
+
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        // take user input
+        System.out.print("Enter the number : ");
+        int number = sc.nextInt();
+
+        // Call printFibonacciSeries() function
+        printFibonacciSeries(number);
+
+        sc.close();
     }
 }
