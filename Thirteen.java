c381c3f6da56883c912e05d7e65d60b6cb109035
gajendra-ingle <gajendraingle01@gmail.com>
2023-10-19 18:44:18 +0530

    Thirteen Day update

--- Thirteen.java
+++ Thirteen.java
@@ -1,18 +1,35 @@
+import java.util.Scanner;
+
 public class Thirteen {
 
     // function that search an eleement in an array
-
-    static void searchElement(int arr[]){
-
+    static int searchElement(int arr[], int number) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == number) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        int arr[] = {2,5,3,7,8,5,9};
+        int arr[] = { 2, 6, 3, 7, 8, 5, 9 };
 
-        // call the searchElement() function
-        searchElement(arr);
-
+        System.out.print("Enter the element you want to find : ");
+        int number = sc.nextInt();
         
+        // call the searchElement() function
+        int elementFound = searchElement(arr, number);
+
+        if (elementFound != -1) {
+            System.out.println("Element found at locationa : " + elementFound);
+        } else {
+            System.out.println("Element not found.");
+        }
+
+        // Sc closed.
+        sc.close();
     }
 }
