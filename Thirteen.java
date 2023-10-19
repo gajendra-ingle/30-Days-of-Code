807cefbfcfc7d9d101aafbac3aef6fe1901e1234
gajendra-ingle <gajendraingle01@gmail.com>
2023-10-19 18:15:42 +0530

    Thirteen Day update

--- Thirteen.java
+++ Thirteen.java
@@ -1,3 +1,18 @@
 public class Thirteen {
-    
+
+    // function that search an eleement in an array
+
+    static void searchElement(int arr[]){
+
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = {2,5,3,7,8,5,9};
+
+        // call the searchElement() function
+        searchElement(arr);
+
+        
+    }
 }
