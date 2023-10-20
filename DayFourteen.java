38df60df4e936d997dc1fbfa0c488d9cc5eb0072
gajendra-ingle <gajendraingle01@gmail.com>
2023-10-20 12:24:59 +0530

    Fourteen Day

--- DayFourteen.java
+++ DayFourteen.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class DayFourteen {
+    // Function that convert a string to upper case
+    static String uppercase(String str) {
+        // character array
+        char a[] = str.toCharArray();
+
+        for (int i = 0; i < a.length; i++) {
+         // condition
+         if (a[i] >= 'a' && a[i] <= 'z') {
+                a[i] = (char) (a[i] - 32); // type casting
+
+            }
+        }
+        return new String(a);
+    }
+
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter any string which is to be converted to uppercase :");
+        String str = sc.next();
+
+        // call uppercase() function
+        String output = uppercase(str);
+        System.out.println("Uppercase String is : " + output);
+
+        //sc closed
+        sc.close();
     }
 }
