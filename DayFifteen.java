de84a70d1a27a02790a8362db4d4e950f6c71366
gajendra-ingle <gajendraingle01@gmail.com>
2023-10-21 15:23:04 +0530

    Fifteen Day

--- DayFifteen.java
+++ DayFifteen.java
@@ -1,3 +1,31 @@
-public class DayFifteen  {
-    
+import java.util.Scanner;
+
+public class DayFifteen {
+
+    // Function that convert a string to lowercase
+    static String lowercase(String str) {
+    // character array
+    char a[] = str.toCharArray();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (a[i] >= 'A' && a[i] <= 'Z') {
+                a[i] = (char) (a[i] + 32); // type casting
+            }
+        }
+        return new String(a);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter any string which is to be converted to lowercase :");
+        String str = sc.nextLine();
+
+        // call the function
+        String outpuString = lowercase(str);
+        System.out.print("Lowercase String is : " + outpuString);
+
+        // sc close
+        sc.close();
+    }
 }
