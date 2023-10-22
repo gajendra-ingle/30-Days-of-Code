2530e1e69e2855b9489478c08292bb597f6092b5
gajendra-ingle <gajendraingle01@gmail.com>
2023-10-22 08:14:58 +0530

    Add function line

--- DaySixteen.java
+++ DaySixteen.java
@@ -1,22 +1,26 @@
 import java.util.Scanner;
 
 public class DaySixteen {
-    //Function that checks the character vowel or consonent
-    static void checkCharacter(char ch){
-        System.out.println(ch);
+    // Function that checks the character vowel or consonent
+    static void checkCharacter(char ch) {
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            System.out.println("it's a vowel..");
+        } else {
+            System.out.println("it's a consonent..");
+        }
 
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        //take user input
-        System.out.println("Enter any one character : ");
+        // take user input
+        System.out.print("Enter any one character : ");
         char ch = sc.next().charAt(0);
 
         checkCharacter(ch);
 
-        //sc closed
+        // sc closed
         sc.close();
     }
 
