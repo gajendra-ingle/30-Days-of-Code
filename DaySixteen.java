b094d8390245872a8f88b4bd8b37670e59eda06e
gajendra-ingle <gajendraingle01@gmail.com>
2023-10-22 08:09:50 +0530

    Add Some line of code in sixteen day file

--- DaySixteen.java
+++ DaySixteen.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
+
 public class DaySixteen {
-    
+    //Function that checks the character vowel or consonent
+    static void checkCharacter(char ch){
+        System.out.println(ch);
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //take user input
+        System.out.println("Enter any one character : ");
+        char ch = sc.next().charAt(0);
+
+        checkCharacter(ch);
+
+        //sc closed
+        sc.close();
+    }
+
 }
