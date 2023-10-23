6b115025cea57a8ae215751e7a5f570d705a0f0d
gajendra-ingle <gajendraingle01@gmail.com>
2023-10-23 08:27:59 +0530

    add some line of code

--- DaySeventeen.java
+++ DaySeventeen.java
@@ -1,3 +1,34 @@
+import java.util.Scanner;
+
 public class DaySeventeen {
-    
+
+    // function that reverse a number
+    static int reverseNumber(int num) {
+        int reverse = 0;
+        int remainder;
+
+        // condition
+        while (num > 0) {
+            remainder = num % 10;
+            reverse = (reverse * 10) + remainder;
+            num = num / 10;
+        }
+        return reverse;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        // take user input
+        System.out.print("Enter the number that you want to reverse : ");
+        int num = sc.nextInt();
+
+        //Print the output on the console      
+        System.out.print("Reverse number is : " + reverseNumber(num));
+
+
+        // sc closed
+        sc.close();
+    }
+
 }
