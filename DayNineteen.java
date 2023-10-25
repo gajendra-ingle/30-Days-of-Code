193b868d06514f21fff4063f1c8b3a884dba2c69
gajendra-ingle <gajendraingle01@gmail.com>
2023-10-25 09:48:26 +0530

    Nineteen day code is add

--- DayNineteen.java
+++ DayNineteen.java
@@ -1,4 +1,34 @@
 public class DayNineteen {
+    // Function that copy one array into another array.
+    static void copyArrayElements(int firstArray[], int secondArray[]) {
+        for (int i = 0; i < firstArray.length; i++) {
+            secondArray[i] = firstArray[i];
+        }
 
-    
+    }
+
+    public static void main(String[] args) {
+
+        // Original array
+        int firstArray[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+        // Print original array
+        System.out.print("Elements of original array : ");
+        for (int i = 0; i < firstArray.length; i++) {
+            System.out.print(firstArray[i] + " ");
+        }
+
+        // copied array
+        int secondArray[] = new int[firstArray.length];
+
+        // call the copyArrayElements() function
+        copyArrayElements(firstArray, secondArray);
+        System.out.println();
+        System.out.print("Elements of copied array : ");
+
+        for (int i = 0; i < secondArray.length; i++) {
+            System.out.print(secondArray[i] + " ");
+        }
+    }
+
 }
