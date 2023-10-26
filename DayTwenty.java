b2ae2ea1d5f24901e0b953de19be47ad603ec8f7
gajendra-ingle <gajendraingle01@gmail.com>
2023-10-26 17:56:00 +0530

    add some line of code

--- DayTwenty.java
+++ DayTwenty.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class DayTwenty {
     //function that calculate the square root of the given number
     static int calculateSquareRoot(int num){
@@ -5,6 +7,18 @@ public class DayTwenty {
         return squareRoot;
     }
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //take user input
+        System.out.print("Enter the number : ");
+        int num = sc.nextInt();
+
+        //call the calculateSquareRoot() function
+        int squareRoot = calculateSquareRoot(num);
+
+        System.out.print("The square root of given number is : "+squareRoot);
+
+        
         
     }
 
