000857e529fbf5e1692842a42a45f1052873a113
gajendra-ingle <gajendraingle01@gmail.com>
2023-10-26 17:50:24 +0530

    Twenty Day

---
+++ DayTwenty.java
@@ -0,0 +1,11 @@
+public class DayTwenty {
+    //function that calculate the square root of the given number
+    static int calculateSquareRoot(int num){
+        int squareRoot =num*num;
+        return squareRoot;
+    }
+    public static void main(String[] args) {
+        
+    }
+
+}
