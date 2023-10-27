97af9c7c5aa7e3a4cd1231852b602de0bb4e500a
gajendra-ingle <gajendraingle01@gmail.com>
2023-10-27 18:06:32 +0530

    Some line of code is added

--- DayTwentyOne.java
+++ DayTwentyOne.java
@@ -1,3 +1,24 @@
+
 public class DayTwentyOne {
-    
+
+    // function that remove space
+    static int removeSpaces(char[] str) {
+        int count = 0; // to track spaces seen so far
+
+        for (int i = 0; i < str.length; i++)
+            if (str[i] != ' ') { // if whitespace is present
+                str[count] = str[i]; // remove whitespace
+                count++; // increment count
+            }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        char[] str = "Take you forward ".toCharArray();
+        int count = removeSpaces(str);
+        System.out.println(String.valueOf(str).subSequence(0, count));
+
+    }
 }
