3ba9c6cf573e1743c25ec0d640c6522b04d3cd03
gajendra-ingle <gajendraingle01@gmail.com>
2023-10-28 10:28:44 +0530

    22 Day

--- DayTwentyTwo.java
+++ DayTwentyTwo.java
@@ -1,3 +1,31 @@
 public class DayTwentyTwo {
+    public static void count(String str, int length) {
+
+        int vowels = 0;
+        int consonants = 0;
+        int whitespaces = 0;
+
+        str = str.toLowerCase(); // converting given string to lowercase
+
+        for (int i = 0; i < length; i++) {
+          char ch = str.charAt(i);
+         
+          if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') 
+            vowels++;
+          else if (ch >= 'a' && ch <= 'z') 
+            consonants++;
+          else if (ch == ' ')
+            whitespaces++;
+        }
     
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Consonants: " + consonants);
+        System.out.println("White spaces: " + whitespaces);
+      }
+      public static void main(String args[]) {
+        String str = "Take u forward is Awesome";
+        int length = str.length();
+        // call the function
+        count(str, length);
+      }
 }
