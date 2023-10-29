bc257e1f41a140d674c3d498fd8c025275aaae84
gajendra-ingle <gajendraingle01@gmail.com>
2023-10-29 18:19:53 +0530

    23 day is added

--- DayTwentyThree.java
+++ DayTwentyThree.java
@@ -1,9 +1,15 @@
+
 public class DayTwentyThree {
 
-    
+
     public static void areaOfCircle(int n) {
 
         double ans = 3.14 * n * n; // Area of circle = πr2
         System.out.print("Area of circle is : " + ans);
+    }
+    public static void main(String[] args) {
+
+        int n = 4;
+        areaOfCircle(n);
     }
 }
