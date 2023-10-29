2d98b00ca818fc7b83a591d4deb180902627f8ff
gajendra-ingle <gajendraingle01@gmail.com>
2023-10-29 18:01:25 +0530

    area of circle function is add

--- DayTwentyThree.java
+++ DayTwentyThree.java
@@ -1,3 +1,9 @@
 public class DayTwentyThree {
+
     
+    public static void areaOfCircle(int n) {
+
+        double ans = 3.14 * n * n; // Area of circle = πr2
+        System.out.print("Area of circle is : " + ans);
+    }
 }
