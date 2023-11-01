38fb15cff04f08a3330f39ebddfb5abe84f95a87
gajendra-ingle <gajendraingle01@gmail.com>
2023-11-01 09:57:37 +0530

    25 day code is add

--- DayTwentySix.java
+++ DayTwentySix.java
@@ -1,6 +1,28 @@
 public class DayTwentySix {
-    
+
+    // function that swap two number
+    static void swapNumber(int num1, int num2) {
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
+
+        // Print the swap number
+        System.out.println("After swapping:");
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+    }
+
     public static void main(String[] args) {
-        
+        int num1 = 5;
+        int num2 = 6;
+
+        // Print original number
+        System.out.println("Before swapping:");
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+
+        // Function call
+        swapNumber(num1, num2);
+
     }
 }
