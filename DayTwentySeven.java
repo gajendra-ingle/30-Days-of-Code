bce0cb1deaaa7dbae724839474d48d987ac93bf4
gajendra-ingle <gajendraingle01@gmail.com>
2023-11-02 19:07:17 +0530

    Some code added

--- DayTwentySeven.java
+++ DayTwentySeven.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class DayTwentySeven {
+
+    // Function that check greater number among the four number.
+    static void checkGreaterNumber(int a, int b, int c, int d) {
+       // Logic
+       
+    }
+
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        // Take user input.
+        System.out.print("Enter the four numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+
+         // Function call.
+        checkGreaterNumber(a, b, c, d);
+
+        // sc closed
+        sc.close();
     }
 }
