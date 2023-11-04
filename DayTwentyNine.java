fdae643310ff80c1bd53641c040265a5bcfdad5e
gajendra-ingle <gajendraingle01@gmail.com>
2023-11-04 09:03:00 +0530

    some changes in 29 day file

--- DayTwentyNine.java
+++ DayTwentyNine.java
@@ -1,15 +1,15 @@
 public class DayTwentyNine {
     // Function to print number triangle pattern
     public static void printPattern(int n) {
-        int i, j;
+       
         // outer loop to handle number of rows
-        for (i = 1; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
             // inner loop to print space
-            for (j = 1; j <= n - i; j++) {
+            for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
             // inner loop to print star
-            for (j = 1; j <= i; j++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print(i + " ");
             }
             // print new line for each row
