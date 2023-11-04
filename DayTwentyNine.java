63dba60fffc02771ec817e68ff9a69964b07d3c8
gajendra-ingle <gajendraingle01@gmail.com>
2023-11-04 09:01:36 +0530

    Forma

--- DayTwentyNine.java
+++ DayTwentyNine.java
@@ -1,28 +1,27 @@
 public class DayTwentyNine {
-    // Function to print number triangle pattern 
-    public static void printPattern(int n) 
-    { 
-        int i, j; 
-        // outer loop to handle number of rows 
-        for (i = 1; i <= n; i++) { 
-            // inner loop to print space 
-            for (j = 1; j <= n - i; j++) { 
-                System.out.print(" "); 
-            } 
-            // inner loop to print star 
-            for (j = 1; j <= i; j++) { 
-                System.out.print(i + " "); 
-            } 
-            // print new line for each row 
-            System.out.println(); 
-        } 
-    } 
+    // Function to print number triangle pattern
+    public static void printPattern(int n) {
+        int i, j;
+        // outer loop to handle number of rows
+        for (i = 1; i <= n; i++) {
+            // inner loop to print space
+            for (j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // inner loop to print star
+            for (j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+            }
+            // print new line for each row
+            System.out.println();
+        }
+    }
 
     public static void main(String[] args) {
-        
+
         int n = 7;
         // Function call
         printPattern(n);
-        
+
     }
 }
